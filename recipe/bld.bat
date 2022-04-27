18958a89b543b61a9b78bf3db817af7bc5be5467
leej3 <johnleenimh@gmail.com>
2022-04-27 13:44:19 +0000

    use ninja on windows

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -1,7 +1,7 @@
 
 mkdir build
 cd build
-cmake ${CMAKE_ARGS} -DWITH_LCMS2:bool=on -DBUILD_MAN:bool=off -DBUILD_SHARED_LIBS:bool=on -DCMAKE_INSTALL_PREFIX:path="$PREFIX" ..
+cmake -LAH -GNinja ${CMAKE_ARGS} -DWITH_LCMS2:bool=on -DBUILD_MAN:bool=off -DBUILD_SHARED_LIBS:bool=on -DCMAKE_INSTALL_PREFIX:path="$PREFIX" ..
 cmake --build . --config Release --target install
 cd ..
 
